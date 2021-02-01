2c0f6bd1fa61d7cedd449322a342231af3c84ce6
asfian99 <mk.asfian@gmail.com>
2021-02-01 11:23:50 +0700

    add chakra-ui

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { connectToDatabase } from "../../utils/mongodb";
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+
+  const users = await db.collection("users").find().limit(10).toArray();
+
+  res.status(200).json(users);
+};
